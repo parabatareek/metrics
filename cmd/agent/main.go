1246add0f528a5ffdfa55f27e16edeabf279a6ae
parabatareek <gavrosh07@gmail.com>
2022-09-24 14:39:53 +0300

    add: *метод чтения обновленных значений метрик

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/parabatareek/metrics.git/internal/metrics"
+	"reflect"
 	"time"
 )
 
 const (
 	pollInterval   = 2 * time.Second
 	reportInterval = 10 * time.Second
+	endpoint       = "127.0.0.1:8080"
 )
 
 func main() {
 	// Инициализация канала
-	channel := make(chan *metrics.Metrics)
-	defer close(channel)
+	getChannel := make(chan *metrics.Metrics)
+	defer close(getChannel)
 
 	// Инициализация структуры Metrics значениями runtime
 	dataMetrics := metrics.NewMetrics()
@@ -28,9 +30,9 @@ func main() {
 	// Когда вы помещаете данные в канал, горутина блокируется до тех пор, пока данные не будут считаны
 	// другой горутиной из этого канала.
 	//https://habr.com/ru/post/490336/
-	go runGetStats(dataMetrics, channel)
+	go runGetStats(dataMetrics, getChannel)
 
-	runSendStats(channel)
+	runReadStats(getChannel)
 }
 
 // Обновление значений объекта Metrics
@@ -43,12 +45,26 @@ func runGetStats(dataMetrics *metrics.Metrics, channel chan *metrics.Metrics) {
 	}
 }
 
-func runSendStats(channel chan *metrics.Metrics) {
+// Чтение значений обновленного объекта Metrics
+func runReadStats(channel chan *metrics.Metrics) {
 	ticker := time.NewTicker(reportInterval)
 	var dataMetrics *metrics.Metrics
 	for {
 		<-ticker.C
 		dataMetrics = <-channel
-		fmt.Println(dataMetrics.PollCount)
+		runSendStats(dataMetrics)
+	}
+}
+
+// Подготовка и отправка данных на сервер
+func runSendStats(dataMetrics *metrics.Metrics) {
+	statType := reflect.TypeOf(dataMetrics).Elem()
+	statVal := reflect.ValueOf(dataMetrics).Elem()
+
+	for i := 0; i < statType.NumField(); i++ {
+		fieldKind := statVal.Field(i).Kind()
+		fieldName := statType.Field(i).Name
+		fieldVal := statVal.Field(i).Interface()
+		fmt.Println(fieldKind, fieldName, fieldVal)
 	}
 }
