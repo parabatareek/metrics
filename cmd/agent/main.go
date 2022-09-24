f10143222b7a0f0a89e30c7f7768d0319ba5e1fa
parabatareek <gavrosh07@gmail.com>
2022-09-24 17:26:19 +0300

    add: *Подготовка и отправка данных на сервер: func runSendStats

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"github.com/parabatareek/metrics.git/internal/metrics"
+	"log"
+	"net/http"
+	"net/url"
 	"reflect"
+	"strconv"
 	"time"
 )
 
@@ -61,10 +67,40 @@ func runSendStats(dataMetrics *metrics.Metrics) {
 	statType := reflect.TypeOf(dataMetrics).Elem()
 	statVal := reflect.ValueOf(dataMetrics).Elem()
 
+	data := url.Values{}
+	urlStr := "/update/"
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
 	for i := 0; i < statType.NumField(); i++ {
 		fieldKind := statVal.Field(i).Kind()
 		fieldName := statType.Field(i).Name
-		fieldVal := statVal.Field(i).Interface()
-		fmt.Println(fieldKind, fieldName, fieldVal)
+		fieldVal := getStrValue(statVal.Field(i))
+
+		params := fmt.Sprintf("<%s>/<%s>/<%s>", fieldKind, fieldName, fieldVal)
+		urlStr += params
+
+		data.Set("url", urlStr)
+
+		request, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBufferString(data.Encode()))
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
+
+		request.Header.Add("Content-Type", "text/plain")
+		request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
 	}
+}
+
+// Приведение типов reflect.Value к float64
+func getStrValue(fieldVal reflect.Value) (val string) {
+	switch fieldVal.Kind() {
+	case reflect.Uint32, reflect.Uint64:
+		val = string(fieldVal.Uint())
+	case reflect.Float32, reflect.Float64:
+		val = strconv.FormatFloat(fieldVal.Float(), 'f', -1, 64)
+	}
+	return
 }
