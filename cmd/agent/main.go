4909e3d692be37e70432247c1f22beee8b32a456
parabatareek <gavrosh07@gmail.com>
2022-09-28 21:33:30 +0300

    change: *рефакторинг

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -22,43 +22,77 @@ const (
 )
 
 func main() {
-	// Инициализация канала
-	updParamChan := make(chan *metrics.Metrics)
-	defer close(updParamChan)
-
 	// Инициализация структуры Metrics значениями runtime
 	dataMetrics := metrics.NewMetrics()
 
 	// Вызов обновления значений объекта Metrics в гоурутине.
-	// Когда вы помещаете данные в канал, горутина блокируется до тех пор, пока данные не будут считаны
-	// другой горутиной из этого канала.
-	//https://habr.com/ru/post/490336/
-	go updStats(dataMetrics, updParamChan)
+	go updStats(dataMetrics)
 
-	// Чтение обновленных данных runtime в гоурутине
-	go readStats(updParamChan)
+	// Отправка данных в гоурутине
+	go sendStats(dataMetrics)
+}
 
+// Обновление значений объекта Metrics
+func updStats(dataMetrics *metrics.Metrics) {
+	ticker := time.NewTicker(pollInterval)
+	<-ticker.C
+
+	dataMetrics.Update()
+	updStats(dataMetrics)
+}
+
+func sendStats(datametrics *metrics.Metrics) {
+	ticker := time.NewTicker(reportInterval)
+	<-ticker.C
+
+	// Формирование данных для отправки
+	urlData := getParams(datametrics)
+
+	// Формирование request
+	request := getRequest(urlData)
+
+	getResponse(request)
+	sendStats(datametrics)
+}
+
+// Формирование данных для отправки
+func getParams(dataMetrics *metrics.Metrics) *url.Values {
+	statType := reflect.TypeOf(dataMetrics).Elem()
+	statVal := reflect.ValueOf(dataMetrics).Elem()
+
+	urlData := url.Values{}
+
+	for i := 0; i < statType.NumField(); i++ {
+		fieldKind := statVal.Field(i).Kind()
+		fieldName := statType.Field(i).Name
+		fieldVal := statVal.Field(i)
+
+		params := fmt.Sprintf("<%v>/<%s>/<%v>", fieldKind, fieldName, fieldVal)
+
+		urlData.Set(fieldName, params)
+	}
+	return &urlData
+}
+
+func getRequest(urlData *url.Values) *http.Request {
 	// Инициализация контекста
 	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
 	defer cancel()
 
-	// Инициализация клиента
-	client := &http.Client{}
-
-	select {
-	case stats := <-updParamChan:
-		urlData := setParams(stats)
-		sendData(ctx, urlData, client)
-	}
-}
-
-func sendData(ctx context.Context, urlData *url.Values, client *http.Client) {
+	// Инициализация запроса
 	request, err := http.NewRequestWithContext(ctx, http.MethodPost, endpoint, bytes.NewBufferString(urlData.Encode()))
 	if err != nil {
 		log.Fatal(err)
 	}
 	request.Header.Add("Content-Type", "text/plain")
 	request.Header.Add("Content-Length", strconv.Itoa(len(urlData.Encode())))
+
+	return request
+}
+
+func getResponse(request *http.Request) {
+	// Инициализация клиента
+	client := &http.Client{}
 
 	// Отпавка данных
 	response, err := client.Do(request)
@@ -72,43 +106,4 @@ func sendData(ctx context.Context, urlData *url.Values, client *http.Client) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-// Обновление значений объекта Metrics
-func updStats(dataMetrics *metrics.Metrics, channel chan *metrics.Metrics) {
-	ticker := time.NewTicker(pollInterval)
-	for {
-		<-ticker.C
-		dataMetrics.Update()
-		channel <- dataMetrics
-	}
-}
-
-// Чтение значений обновленного объекта Metrics
-func readStats(channel chan *metrics.Metrics) {
-	ticker := time.NewTicker(reportInterval)
-	//var dataMetrics *metrics.Metrics
-	for {
-		<-ticker.C
-		<-channel
-	}
-}
-
-func setParams(dataMetrics *metrics.Metrics) *url.Values {
-	statType := reflect.TypeOf(dataMetrics).Elem()
-	statVal := reflect.ValueOf(dataMetrics).Elem()
-
-	urlData := url.Values{}
-
-	for i := 0; i < statType.NumField(); i++ {
-		fieldKind := statVal.Field(i).Kind()
-		fieldName := statType.Field(i).Name
-		fieldVal := statVal.Field(i)
-
-		params := fmt.Sprintf("<%v>/<%s>/<%v>", fieldKind, fieldName, fieldVal)
-		//urlParams := urlUpdate + params
-
-		urlData.Set(fieldName, params)
-	}
-	return &urlData
 }
